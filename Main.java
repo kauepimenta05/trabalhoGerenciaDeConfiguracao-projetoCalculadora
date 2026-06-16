cb9a7f7692bcad4ae7f64fb64b4ea7454d8c640c
Kauê Pimenta <kauegabrielp2005@gmail.com>
2026-06-16 15:48:41 -0300

    Estrutura inicial da calculadora em Java - Baseline 1

---
+++ Main.java
@@ -0,0 +1,16 @@
+// Projeto: Calculadora do Grupo do Kauê
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        exibirMenu();
+    }
+
+    public static void exibirMenu() {
+        System.out.println("=== CALCULADORA DO GRUPO ===");
+        System.out.println("1. Soma / Subtração (Em desenvolvimento)");
+        System.out.println("2. Multiplicação / Divisão (Em desenvolvimento)");
+        System.out.println("3. Sair");
+        System.out.println("============================");
+    }
+}
